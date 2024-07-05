743d6bbeb06b5d4a5cbf63cd96a05d13c2fa0e5f
Jiali Xuan <v-xuanjiali@microsoft.com>
2024-07-05 13:35:31 +0800

    update

--- src/utils/markdeep-common.js
+++
@@ -1,4 +0,0 @@
-     window.alreadyProcessedMarkdeep || (document.body.onload = () => {
-         markdeepOptions = { tocStyle: 'medium', toc: true };
-         window.processMarkdeep && processMarkdeep();
-     }, window.alreadyProcessedMarkdeep = true);
